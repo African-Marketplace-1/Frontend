eddb6241765d0490dbbda073d3d76e7d6dad966a
Dave Betts <ibedave777@gmail.com>
2020-02-04 12:36:53 -0500

    Signin Form functionality completed

--- afr-marketplace/src/components/SignIn.js
+++ afr-marketplace/src/components/SignIn.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/SignIn.css';
+import { axiosWithAuth } from '../utils/AxiosWithAuth';
+
 
 const SignIn = (props) => {
+    
+    const [credentials, setCredentials] = useState({});
+
+    const signin = event => {
+        event.preventDefault();
+        axiosWithAuth().post("https://african-marketplace-2020.herokuapp.com/api/auth/login", credentials)
+        .then(response => {
+            console.log(response.data)
+            localStorage.setItem('token', response.data.token);
+            props.history.push('/');
+        })
+    }
+
+    const handleChange = event => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+        console.log(credentials);
+    }
+      
+        
     return (
         <div className='container'>
             <h1 className='title'>Welcome back to Sauti Africa!</h1>
             <div className='sign-in-card'>   
-                <form>
-                    <label htmlFor='email' className='form-label'>
-                        Email Address
-                    </label>
-                    <input type='email' className='form-input'/>
+                <form onSubmit={signin}>
+                    <label className='form-label'>Username</label>
+                    <input 
+                        label="username"
+                        type="text"
+                        placeholder="username" 
+                        className="form-input"
+                        name="username"
+                        onChange={handleChange}
+                    />
                     <div className='password'>
-                        <div><label htmlFor='password' className='form-label'>Password</label></div> 
+                        <div>
+                            <label htmlFor='password' className='form-label'>Password</label>
+                        </div> 
                         <div className='forgot-password'><a href='#'>Forgot password?</a></div>
                     </div>
-                    <input type='password' className='form-input'/>
+                    <input
+                        label="password"
+                        placeholder="password"
+                        type='password' 
+                        name="password"
+                        className='form-input'
+                        onChange={handleChange}
+                    />
                     <button className='sign-in'>Sign in</button>
                 </form>
                 <p className='new-user'>New user? <a href='#'>Create an account.</a></p>

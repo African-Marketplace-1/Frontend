ab5910a9347ac01cdfb8dd98c4a80786d0418ae3
hlee2542 <ou40a9rtbp@gmail.com>
2020-02-04 13:21:56 -0800

    added form validation to new item page

--- afr-marketplace/src/components/NewItemForm.js
+++ afr-marketplace/src/components/NewItemForm.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './NewItem.css'
 
 const NewItem = (props) => {
+    let [name, setName] = useState('');
+    let [description, setDescription] = useState('');
+    let [price, setPrice] = useState('');
+    let [canPass, setCanPass] = useState(true);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (name.length === 0 || description.length === 0) {
+            setCanPass(false);
+            return false;
+        }
+        else if (!price) {
+            setCanPass(false);
+            return false;
+        }
+        else if (isNaN(price)) {
+            setCanPass(false);
+            return false;
+        }
+        //submit
+        setCanPass(true);
+        return true;
+    };
     return (
         <div>
             <h1 className='title'>Add an Item</h1>
-            <form>
+            <p className='error'>{canPass ? null : "You need a name and a description, and the price must be a number."}</p>
+            <form onSubmit={(event) => handleSubmit(event)}>
                 <select className='entry'>
                     <option>Somalia</option>
                     <option>Ethiopia</option>
@@ -14,9 +37,9 @@ const NewItem = (props) => {
                     <option>Uganda</option>
                     <option>Tanzania</option>
                 </select>
-                <input type='text' placeholder='Item Name' className='entry'/>
-                <textarea  className='entry description' placeholder='Type in a description.'></textarea>
-                <input type='text' placeholder='Price' className='entry'/>
+                <input type='text' placeholder='Item Name' className='entry' value={name} onChange={(event) => setName(event.target.value)}/>
+                <textarea  className='entry description' placeholder='Type in a description.' value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
+                <input type='text' placeholder='Price' className='entry' value={price} onChange={(event) => setPrice(event.target.value)}/>
                 <select className='entry'>
                     <option>Animal Products</option>
                     <option>Dry Goods</option>
@@ -37,6 +60,7 @@ const NewItem = (props) => {
                     <option>Greens</option>
                     <option>Smokables</option>
                 </select>
+                <button className='submit'>Submit</button>
             </form>
         </div>
     )

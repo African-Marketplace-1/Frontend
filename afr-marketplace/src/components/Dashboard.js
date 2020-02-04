3a86e25771b1f8d98c566d8c5b2152731862103e
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-04 12:59:51 -0500

    Getting data displayed on dashboard.

--- afr-marketplace/src/components/Dashboard.js
+++ afr-marketplace/src/components/Dashboard.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+// axiosWithAuth
+import { axiosWithAuth } from '../utils/AxiosWithAuth.js';
+// Context
+import { UserContext } from '../contexts/UserContext.js';
 
 const Dashboard = (props) => {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        axiosWithAuth().get('api/prices')
+            .then((response) => {
+                console.log("This is response from Dashboard:", response.data)
+                setItems(response.data)
+            })
+            .catch((error) => {
+                console.log("This is an error from Dashboard:", error)
+            })
+    }, [])
+
     return (
+
         <div>
-            hello, from dashboard component.
+            {
+                items.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <h1>{item.product}</h1>
+                            <h3>${item.avg_price}</h3>
+                            <h6>{item.product_cat}</h6>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
